ccaaec30a73075476a77448ec5894c7df54d3be1
buntu <teklufe@gmail.com>
2014-04-19 18:13:57 +0200

    comment model finished

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Blog::Application.routes.draw do
   get "welcome/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-	resources :articles
+	resources :articles do
+		resources :comments
+	end
   # You can have the root of your site routed with "root"
    root 'welcome#index'
 
